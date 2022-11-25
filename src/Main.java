fb8bd1e579bbb277e01f4c2676c89bfbdcbf9110
Ssemgame <81359690+Ssemgame@users.noreply.github.com>
2022-11-25 16:44:49 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    void send(String str){
+        System.out.println(str);
+    }
+    void input() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String out = input.readLine();
+    }
+    void convertInt(int ){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
